37832205d0152e6eafc48956e8644cfb9502fd97
PiHan Hsu <pihanhsu@gmail.com>
2016-03-18 16:32:56 +0800

    show username on index page. hide edit/delete if user is not author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316041459) do
+ActiveRecord::Schema.define(version: 20160318025117) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160316041459) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
